b2d0252c8fecd91134b7c19c7e9cc5edfb35a9d6
Amir Sejdinovic <amir24V@yahoo.com>
2020-12-18 09:20:42 +0100

    Creating gulp tasks for minify images and copy them

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,6 +5,7 @@ import gulp from 'gulp';
 import cleanCSS from 'gulp-clean-css';
 import gulpif from 'gulp-if';
 import sorucemaps from 'gulp-sourcemaps';
+import imagemin from 'gulp-imagemin';
 
 const PRODUCTION = yargs.argv.prod;
 
@@ -12,6 +13,14 @@ const paths = {
   styles: {
     src: ['./src/assets/scss/bundle.scss','./src/assets/scss/admin.scss'],
     dest: 'dist/assets/css'
+  },
+  images: {
+     src: './src/assets/images/**/*.{jpg,jpeg,png,svg,gif}',
+     dest: 'dist/assets/images'
+  },
+  other: {
+     src: ['src/assets/**/*','!src/assets/{images,js,scss}', '!src/assets/{images,js,scss}/**/*'],
+     dest: 'dist/assets'
   }
 }
 
@@ -41,10 +50,23 @@ function watch(){
   gulp.watch('src/assets/scss/*.scss', styles);
 }
 
+function images(){
+  return gulp.src(paths.images.src)
+  .pipe(gulpif(PRODUCTION, imagemin()))
+  .pipe (gulp.dest(paths.images.dest));
+}
+
+
+function copy(){
+  return gulp.src(paths.other.src)
+  .pipe(gulp.dest(paths.other.dest));
+}
 
 
 exports.styles = styles;
 exports.watch = watch;
+exports.images = images;
+exports.copy = copy;
 
 exports.default = defaultTask
 
