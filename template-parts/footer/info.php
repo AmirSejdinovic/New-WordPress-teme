88fc98426701f4ba350ff60a1da805d5f31ab1ea
Amir Sejdinovic <amir24V@yahoo.com>
2021-01-15 09:32:16 +0100

    Addin options tu the custimizer

--- template-parts/footer/info.php
+++ template-parts/footer/info.php
@@ -1,7 +1,10 @@
+<?php
+    $site_info = get_theme_mod('_themename_site_info', '');
+?>
 <div class="c-site-info">
      <div class="row">
         <div class="container">
-          All Right Recived
+          <?php echo esc_html($site_info); ?>
         </div>
      </div>
     </div>
